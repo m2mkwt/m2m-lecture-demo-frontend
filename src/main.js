d2b225cd6a1e318e7283522dd7d1c9600af3eb12
eunmin <unmin9412@gmail.com>
2022-03-28 14:53:44 +0900

    axios 설정 및 예제 추가

--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 import router from './router.js'
+import axios from 'axios'
 
+axios.defaults.baseURL = '';
+axios.defaults.headers.get['Content-Type'] = 'application/json;charset=utf-8';
+axios.defaults.headers.get['Access-Control-Allow-Origin'] = '*';
 
 const app = createApp(App)
 app.use(router)
+// axios 전역변수 설정
+app.config.globalProperties.axios = axios
 app.mount('#app')
