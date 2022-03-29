9dbe8cba7a92f49591bb51123bd5d9234e5f2414
Joorano <jooryeonh@gmail.com>
2022-03-29 13:59:40 +0900

    FIX: 프로필 편집 페이지 수정

--- src/components/ProfileEdit.vue
+++ src/components/ProfileEdit.vue
@@ -1,34 +1,138 @@
 <template>
     <the-header></the-header>
-    <div class="bg-zinc-50 min-h-screen">
+    <div class="bg-zinc-50 min-h-screen overflow-y-auto">
         <div class="max-w-4xl mx-auto pt-24">
-            <div class="bg-white w-full border h-screen flex">
+            <div class="bg-white w-full border flex">
                 <!-- aside menu -->
-                <div class="w-1/4 border-r px-8 py-3 space-y-5 h-full">
-                    <div>프로필 편집</div>
-                    <div>비밀번호 변경</div>
-                    <div>이메일 및 SMS</div>
+                <div class="w-1/4 border-r hidden md:block">
+                    <div class="py-4 px-7 cursor-pointer border-l-2 border-black w-full font-semibold">프로필 편집</div>
+                    <div class="py-4 px-7 cursor-pointer">비밀번호 변경</div>
+                    <div class="py-4 px-7 cursor-pointer">이메일 및 SMS</div>
                 </div>
                 <!-- content -->
-                <div class="flex flex-1 py-3">
-                    <div class="w-1/4 flex flex-col">
-                        <div>이름</div>
-                        <div>사용자 이름2</div>
-                        <div>웹사이트</div>
-                        <div>소개</div>
+                <div class="flex flex-1 py-6 pr-32">
+                    <div class="space-y-4">
+                        <!-- 1st row -->
+                        <div class="flex space-x-6 items-center">
+                            <div class="w-40 flex justify-end">
+                                <div class="cursor-pointer w-10">
+                                    <img src="../assets/images/avatar.jpg">
+                                </div>
+                            </div>
+
+                            <div class="flex-1">
+                                <div class="text-xl">UserId</div>
+                                <div class="cursor-pointer text-blue-500 text-sm font-semibold">프로필 사진 바꾸기</div>
+                            </div>
+
+
+                        </div>
+
+                        <!-- 2nd row -->
+                        <div class="flex space-x-6">
+                            <div class="w-40 flex justify-end pt-2">
+                                <div class="font-semibold">이름</div>
+                            </div>
+                            <div class="flex-1 flex flex-col space-y-3">
+                                <div>
+                                    <label class="hidden" for="userName">이름</label>
+                                    <input id="userName" class="rounded focus:outline-none text-sm font-medium py-2 pl-3 w-full border text-left" type="text" value="이름">
+                                </div>
+                                <div class="text-xs text-gray-400">사람들이 이름, 별명 또는 비즈니스 이름 등 회원님의 알려진 이름을 사용하여 회원님의 계정을 찾을 수 있도록 도와주세요.</div>
+                                <div class="text-xs text-gray-400">이름은 14일 안에 두 번만 변경할 수 있습니다.</div>
+                            </div>
+                        </div>
+                        <!-- 3rd row -->
+                        <div class="flex space-x-6">
+                            <div class="w-40 flex justify-end pt-2">
+                                <div class="font-semibold">사용자 이름</div>
+                            </div>
+                            <div class="flex-1 flex flex-col space-y-3">
+                                <div>
+                                    <label class="hidden" for="userName">이름</label>
+                                    <input id="userName" class="rounded focus:outline-none text-sm font-medium py-2 pl-3 w-full border text-left" type="text" value="이름">
+                                </div>
+                                <div class="text-xs text-gray-400">대부분의 경우 14일 이내에 사용자 이름을 다시 joo7289(으)로 변경할 수 있습니다.</div>
+
+                            </div>
+                        </div>
+                        <!-- 4th row -->
+                        <div class="flex space-x-6">
+                            <div class="w-40 flex justify-end pt-2">
+                                <div class="font-semibold">소개</div>
+                            </div>
+                            <div class="flex-1 flex flex-col space-y-3">
+                                <div>
+                                    <label class="hidden" for="userName">소개</label>
+                                    <textarea class="rounded border w-full" name="" id="" rows="3"></textarea>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- 이메일 -->
+                        <div class="flex space-x-6">
+                            <div class="w-40 flex justify-end pt-2">
+                                <div class="font-semibold">이메일</div>
+                            </div>
+                            <div class="flex-1 flex flex-col space-y-3">
+                                <div>
+                                    <label class="hidden" for="userName">이름</label>
+                                    <input id="userName" class="rounded focus:outline-none text-sm font-medium py-2 pl-3 w-full border text-left" type="text" value="이름">
+                                </div>
+                        
+                            </div>
+                        </div>   
+                        <!-- 전화번호 -->
+                        <div class="flex space-x-6">
+                            <div class="w-40 flex justify-end pt-2">
+                                <div class="font-semibold">전화번호</div>
+                            </div>
+                            <div class="flex-1 flex flex-col space-y-3">
+                                <div>
+                                    <label class="hidden" for="userName">이름</label>
+                                    <input id="userName" class="rounded focus:outline-none text-sm font-medium py-2 pl-3 w-full border text-left" type="text" value="이름">
+                                </div>
+                            </div>
+                        </div>
+                        <!-- 성별 -->
+                        <div class="flex space-x-6">
+                            <div class="w-40 flex justify-end pt-2">
+                                <div class="font-semibold">성별</div>
+                            </div>
+                            <div class="flex-1 flex flex-col space-y-3">
+                                <div>
+                                    <label class="hidden" for="userName">이름</label>
+                                    <input id="userName" class="rounded focus:outline-none text-sm font-medium py-2 pl-3 w-full border text-left" type="text" placeholder="성별">
+                                </div>
+                            </div>
+                        </div>
+                        <!-- 제출 버튼 & 비활성화 -->
+                        <div class="flex space-x-6 items-center">
+                            <div class="w-40">
+                                
+                            </div>
+                            <div class="flex-1 flex justify-between items-center">
+                                <button class="bg-blue-500 rounded py-1 px-4 text-white font-semibold">제출</button>
+                                <a class="text-blue-500 text-sm font-semibold" href="#">
+                                    계정을 일시적으로 비활성화
+                                </a>
+                            </div>                            
+                        </div>
                     </div>
                     <div class="flex-1"></div>
                 </div>
             </div>
         </div>
     </div>
+    <the-footer></the-footer>
 </template>
 
 <script>
 import TheHeader from '../components/layout/TheHeader.vue'
+import TheFooter from '../components/layout/TheFooter.vue'
 export default {
   components: {
-    TheHeader
+    TheHeader,
+    TheFooter
   },     
 }
 </script>
