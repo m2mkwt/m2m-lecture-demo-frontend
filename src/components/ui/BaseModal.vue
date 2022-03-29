6f955af65734c7fc8059afc792f38c212c462b35
Joorano <jooryeonh@gmail.com>
2022-03-29 13:54:18 +0900

    FEAT: 공통 ui > Modal Component 추가

---
+++ src/components/ui/BaseModal.vue
@@ -0,0 +1,19 @@
+<template>
+    <teleport to="body" v-show="modalActive">
+        <div @click="close" v-show="modalActive" class="fixed bg-black opacity-50 inset-0 z-40"></div>
+        <div v-show="modalActive" class="absolute inset-0 flex justify-center items-center">
+            <slot></slot>
+        </div>
+    </teleport>
+</template>
+<script>
+export default {
+    props: ["modalActive"],
+    setup(props, { emit }) {
+        const close = () => {
+            emit("close")
+        }
+        return { close }
+    }
+}
+</script>
