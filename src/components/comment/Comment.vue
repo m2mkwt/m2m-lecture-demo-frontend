678044422b09000fa6e9faed13cb0625c3db8be3
Joorano <jooryeonh@gmail.com>
2022-04-01 14:04:51 +0900

    FEAT: 댓글 수정시 input focus 기능 및 keyup.enter 이벤트 바인딩

--- src/components/comment/Comment.vue
+++ src/components/comment/Comment.vue
@@ -4,7 +4,9 @@
     <div class="flex flex-col space-y-4">
         <div class="leading-none">
             <span class="font-semibold text-sm mr-2">{{ memberNo }}</span>
-            <span class="text-sm">{{ contentComment }}</span>
+            <!-- <span class="text-sm">{{ contentComment }}</span> -->
+            <label for="comment" class="hidden">댓글</label>
+            <input @keyup.enter="submitUpdatedComment" v-model="contentComment" class="text-sm focus:outline-none" type="text" ref="commentCont" name="comment" id="comment">
         <button
             class="bg-blue-100 rounded py-1 px-1 text-white font-semibold"
             @click="updateComment">
@@ -31,19 +33,24 @@ export default {
         'postNo',
         'contentComment',
         'deleteYN',
-        'createdt'
+        'createdt',
     ],
     methods: {
         updateComment() {
-            axios.post ("/api/v1/comment/editComment",{
-                memberNo: store.memberNo,
-                postNo: this.postNo,
-                content: this.editConent
-            }).then(res => {
-                this.commentList = res.data.data;
-            }).catch(err => {
-                console.log(err);
-            })
+            this.$refs.commentCont.focus()
+            // axios.post ("/api/v1/comment/editComment",{
+            //     memberNo: store.memberNo,
+            //     postNo: this.postNo,
+            //     content: this.editConent
+            // }).then(res => {
+            //     this.commentList = res.data.data;
+            //     this.$refs.commentCont.focus()
+            // }).catch(err => {
+            //     console.log(err);
+            // })
+        },
+        submitUpdatedComment() {
+            console.log( this.contentComment+'으로 수정되었습니다')
         },
         deleteComment() {
             console.log("포스트 : " + this.commentNo)
