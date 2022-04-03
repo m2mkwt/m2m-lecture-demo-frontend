b7d8fb911869f0d14307c6f167170b3d3e5a4f00
eunmin <unmin9412@gmail.com>
2022-04-03 18:55:15 +0900

    FIX: 헤더 수정
    프로필 → 마이페이지로 수정

--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -37,7 +37,7 @@
                     <div class="w-full h-full px-4">
                       <router-link to="/mypage" class="py-2 flex items-center w-full">
                         <svg class="mr-3" color="#262626" fill="#262626" height="16" role="img" viewBox="0 0 24 24" width="16"><circle cx="12.004" cy="12.004" fill="none" r="10.5" stroke="currentColor" stroke-linecap="round" stroke-miterlimit="10" stroke-width="2"></circle><path d="M18.793 20.014a6.08 6.08 0 00-1.778-2.447 3.991 3.991 0 00-2.386-.791H9.38a3.994 3.994 0 00-2.386.791 6.09 6.09 0 00-1.779 2.447" fill="none" stroke="currentColor" stroke-linecap="round" stroke-miterlimit="10" stroke-width="2"></path><circle cx="12.006" cy="9.718" fill="none" r="4.109" stroke="currentColor" stroke-linecap="round" stroke-miterlimit="10" stroke-width="2"></circle></svg>
-                        <span>프로필</span>
+                        <span>마이페이지</span>
                       </router-link>
                     </div>
                     <div class="px-4 py-2 flex items-center w-full border-t">
